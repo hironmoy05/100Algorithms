038e5371657b9f530f19149675fb3a608dfd04e5
gaurav <gaurav@gmail.com>
2021-08-03 21:09:30 +0530

    Done

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,19 @@
-export function addBorder(picture: string[]): string[] {
+export function addBorder(picture: string[]): any {
+    const wall = "*".repeat(picture[0].length + 2);
+    // let wall = "";
+    
+    // for (let i = 0; i <= displayWall; i++) {
+    //     wall = wall.concat("*");
+    // }
 
+    picture.push(wall);
+    picture.unshift(wall);
+
+    for (let i = 1; i < picture.length - 1; i++) {
+        picture[i] = "*".concat(picture[i] + "*");
+    }
+
+    return picture;
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abc", "ded"]));
--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,17 @@
-export function addTwoDigits(n: any): number {
+// export function addTwoDigits(n: any): number {
 
+// }
+
+// // console.log(addTwoDigits(29));
+
+export function addTwoDigits(n: any): number {
+    const nums = n.toString().split("");
+
+    // return nums.reduce((a: string, b: string) => {
+    //     return parseInt(a) + parseInt(b);
+    // });
+
+    return parseInt(nums[0]) + parseInt(nums[1]);
 }
 
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(29));
